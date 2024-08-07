47bccf9c14e93cb2311e3459ad24875c966cb8f3
Erik <erikellilo@gmail.com>
2024-08-07 22:35:35 +0700

    Styling Input Amount in Calculate

--- src/pages/CalculatePage.jsx
+++ src/pages/CalculatePage.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Form from "../ui/Form";
+import FormRow from "../ui/FormRow";
 
 const CalculateStyled = styled.div`
   display: flex;
@@ -6,9 +8,10 @@ const CalculateStyled = styled.div`
   align-items: flex-start;
   gap: 3rem;
   flex-wrap: wrap;
-  margin: 2rem;
+  margin: 1rem;
   padding: 2rem;
   border: 0.25rem solid black;
+  width: 100%;
 
   position: relative;
   background-color: #f9f9f9;
@@ -41,8 +44,79 @@ const CalculateStyled = styled.div`
   }
 `;
 
+const CalculateContent = styled.div`
+  position: relative;
+  z-index: 1;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 0.75rem;
+  background-color: var(--color-grey-50);
+  height: 5rem;
+  width: 100%;
+  flex-grow: 1;
+`;
+
+const ExpanseCurrency = styled.div`
+  background-color: var(--color-brand-200);
+  padding: 0.3rem 0.5rem 0rem 0.5rem;
+  border: 0.2rem solid var(--color-grey-900);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-width 0.1s ease;
+  h1 {
+    font-size: 2.5rem;
+    color: var(--color-grey-900);
+  }
+
+  ${Input}:hover ~ & {
+    background-color: yellow;
+  }
+`;
+
+const ExpanseContent = styled.div`
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+  width: 100%;
+
+  transition: border-width 0.1s ease;
+  &:focus-within ${ExpanseCurrency} {
+    /* Styles for ExpanseCurrency when input is focused */
+    outline: 2px solid var(--color-grey-900);
+    outline-offset: -1px;
+  }
+`;
+
 const CalculatePage = () => {
-  return <CalculateStyled>CalculatePage</CalculateStyled>;
+  return (
+    <CalculateStyled>
+      <CalculateContent>
+        <h1>Calculate</h1>
+        <Form>
+          <FormRow name="Pengeluaran Untuk">
+            <Input id="expanse" name="expanse" placeholder="Bayarin Apa nih" />
+          </FormRow>
+          <FormRow name="Biaya">
+            <ExpanseContent>
+              <ExpanseCurrency>
+                <h1>Rp</h1>
+              </ExpanseCurrency>
+              <Input
+                id="amount"
+                name="amount"
+                placeholder="Berapa nih?"
+                type="number"
+              />
+            </ExpanseContent>
+          </FormRow>
+        </Form>
+      </CalculateContent>
+    </CalculateStyled>
+  );
 };
 
 export default CalculatePage;
